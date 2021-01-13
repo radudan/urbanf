d4bd19c0c998e247514f36352d3b30e9c07bac8c
radadancristian <radu.dan.uk@gmail.com>
2021-01-13 22:40:32 +0000

    Nu stiu unde :)

--- src/components/CommonComponents.jsx
+++ src/components/CommonComponents.jsx
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import urbanLogo from "./urbanLogo.jpg";
 import Button from "./Button";
+let server = 'urbanfit.tocloud.in';
+// let server = 'localhost';
 
 export function Logo(){
     return (
@@ -44,7 +46,7 @@ export function Title({update, setUpdate, week, setWeek}) {
 }
 
 export  async function performFetch(query){
-    const data = await fetch("http://urbanfit.tocloud.in:3001/" + query);
+    const data = await fetch(`http://${server}:3001/` + query);
     let resp = await data.json()
     //console.log(resp)
     return resp;
